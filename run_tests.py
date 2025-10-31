f1feed2655b26a82032475e14117c33c881860e8
Ana Herrera Flores <ana.herreraflores@g.austincc.edu>
2025-10-31 23:14:25 +0000

    DNA homework

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import unittest
-from tests.homework.e_functions import tests_functions
+from tests.homework.h_strings import tests_strings
 
 if __name__ == "__main__":
-	suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
+	suite = unittest.TestLoader().loadTestsFromModule(tests_strings)
 	unittest.TextTestRunner(verbosity=2).run(suite)
 
